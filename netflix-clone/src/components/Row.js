543d65be6f3df5c0beeb3a98253bca67fdd01c67
Andrew Le <s3820098@rmit.edu.vn>
2021-08-20 21:24:14 +0700

    render the posters from the movies got from API

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,38 +1,50 @@
-import instance from '../axios';
-import React, { useState, useEffect } from 'react';
+import instance from "../axios";
+import React, { useState, useEffect } from "react";
+
+// base url for images
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row(props) {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData () {
-            // await means when you make the request, wait for the request to come back
-            // you created a based url form axios, so .get will attach the baseURL to the request
-            const request = await instance.get(props.fetchUrl);
+  useEffect(() => {
+    async function fetchData() {
+      // await means when you make the request, wait for the request to come back
+      // you created a based url form axios, so .get will attach the baseURL to the request
+      const request = await instance.get(props.fetchUrl);
 
-            // console log to see the STRUCTURE OF THE DATA that you are getting back
-            // console.log(request.data.results);
-            setMovies(request.data.results);
-            return request;
-        }
+      // console log to see the STRUCTURE OF THE DATA that you are getting back
+      // console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
 
-        fetchData();
+    fetchData();
 
-        // if [], run this once when the row loads and dont do it again
-        // if [movies], run once and run again every single time the 'movies' change --> dependency
+    // if [], run this once when the row loads and dont do it again
+    // if [movies], run once and run again every single time the 'movies' change --> dependency
 
-        // [props.fetchUrl] because we use props.fetchUrl inside useEffect, when it is an outisde variable
-    }, [props.fetchUrl])
+    // [props.fetchUrl] because we use props.fetchUrl inside useEffect, when it is an outisde variable
+  }, [props.fetchUrl]);
 
-    // to see if the request using useEffect works
-    console.log(movies);
+  // to see if the request using useEffect works
+  // console.log(movies);
 
-    return (
-        <div>
-            <h2>{props.title}</h2>
-            
-        </div>
-    )
+  return (
+    <div className="row">
+      <h2>{props.title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
